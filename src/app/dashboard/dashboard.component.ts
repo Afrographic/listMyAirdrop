48a01a2e1d649150357427054aee6417662e6908
unknown <bruneltesse@gmail.com>
2022-04-02 16:54:44 +0100

    yeah

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isInViewport } from '../constant';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,18 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.showDivWhenVisible();
+  }
+
+  showDivWhenVisible() {
+    const box:any = document.querySelector('#dashBoard');
+    const body:any = document.querySelector('body');
+    body.addEventListener('scroll', function () {
+      console.log("logo");
+      console.log(isInViewport(box));
+    }, {
+      passive: true
+    });
   }
 
 }
